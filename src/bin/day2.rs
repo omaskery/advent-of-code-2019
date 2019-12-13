b2f7f5021cd910f1e49692228323a3493882e79b
Oliver Maskery <omaskery@googlemail.com>
2019-12-13 22:00:37 +0000

    solution for day 2 part b

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -184,10 +184,34 @@ impl<'a, M: Memory> Computer<'a, M> {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let result = run_gravity_assist_with_parameters(12, 2)?;
+
+    let goal_result = 19690720;
+    let mut necessary_parameters = None;
+    'outer: for noun in 0..=99 {
+        for verb in 0..=99 {
+            let current_result = run_gravity_assist_with_parameters(noun, verb)?;
+            if current_result == goal_result {
+                necessary_parameters = Some((noun, verb));
+                break 'outer;
+            }
+        }
+    }
+
+    println!("initial result in slot 0: {}", result);
+    if let Some((noun, verb)) = necessary_parameters {
+        println!("necessary parameters for part b: noun={} verb={}", noun, verb);
+        println!("  so 100 * noun + verb = {}", 100 * noun + verb);
+    }
+
+    Ok(())
+}
+
+fn run_gravity_assist_with_parameters(noun: i32, verb: i32) -> Result<i32, Box<dyn Error>> {
     let mut memory = SimpleMemory::from_memory_file("input/day2.txt")?;
 
-    memory.write_slot(1, 12).compat()?;
-    memory.write_slot(2, 2).compat()?;
+    memory.write_slot(1, noun).compat()?;
+    memory.write_slot(2, verb).compat()?;
 
     let mut computer = Computer::new(&mut memory);
 
@@ -195,10 +219,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         computer.step().compat()?;
     }
 
-    let result = memory.read_slot(0).compat()?;
-    println!("result in slot 0: {}", result);
-
-    Ok(())
+    Ok(memory.read_slot(0).compat()?)
 }
 
 #[cfg(test)]
